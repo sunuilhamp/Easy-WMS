1a63429af25fa70589c1b219d402885e5c5a08f8
Sunu Ilham Pradika <sunudika@gmail.com>
2020-04-28 17:25:09 +0700

    Add new alert

--- application/views/layouts/_alert.php
+++ application/views/layouts/_alert.php
@@ -1,36 +1,24 @@
 <?php
 
-$success    = $this->session->flashdata('success');
-$error      = $this->session->flashdata('error');
-$warning    = $this->session->flashdata('warning');
+    $success    = $this->session->flashdata('success');
+    $error      = $this->session->flashdata('error');
+    $warning    = $this->session->flashdata('warning');
+?>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<?php if ($success) { ?>
+    <script>
+      swal("Berhasil!", "<?php echo $success ?>","success")
+    </script>
+<?php } ?>
 
-if ($success) {
-    $alert_status   = 'alert-success';
-    $status         = 'Success!';
-    $message        = $success;
-}
+<?php if ($error) { ?>
+    <script>
+      swal("Gagal!", "<?php echo $error ?>","error")
+    </script>
+<?php } ?>
 
-if ($error) {
-    $alert_status   = 'alert-danger';
-    $status         = 'Error!';
-    $message        = $error;
-}
-
-if ($warning) {
-    $alert_status   = 'alert-warning';
-    $status         = 'Warning!';
-    $message        = $warning;
-}
-
-if ($success || $error || $warning) : ?>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="alert <?= $alert_status ?> alert-dismissible fade show" role="alert">
-                <strong><?= $status ?></strong> <?= $message ?>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        </div>
-    </div>
-<?php endif ?>
+<?php if ($warning) { ?>
+    <script>
+      swal("Warning!", "<?php echo $warning ?>","warning")
+    </script>
+<?php } ?>
