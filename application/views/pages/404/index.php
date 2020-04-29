f06b435d80c05c8e9d0cbaac3afdebae70bd399a
Sunu Ilham Pradika <sunudika@gmail.com>
2020-04-29 22:21:32 +0700

    Add login button as alternative in 404

--- application/views/pages/404/index.php
+++ application/views/pages/404/index.php
@@ -26,6 +26,7 @@
 			<h2>Oops! Halaman Tidak Ditemukan</h2>
 			<p>Maaf mungkin halaman yang anda tuju sedang diperbaiki / sudah dihapus / sudah diganti dengan yang lain.</p>
 			<a href="<?= base_url('home') ?>">Back to homepage</a>
+			<a href="<?= base_url('login') ?>">Login</a>
 		</div>
 	</div>
 
